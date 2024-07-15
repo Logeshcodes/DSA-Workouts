d4e0cf2870c201826d2fb819cf283e8127225008
Logesh <logeshc18022002@gmail.com>
2024-07-15 11:43:56 +0530

    practice

---
+++ DataStructure/LinkedList/demo4.js
@@ -0,0 +1,208 @@
+class Node{
+    constructor(value){
+        this.value = value ;
+        this.next = null ;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null ;
+        this.size = 0 ;
+    }
+
+    getSize(){
+        return this.size ;
+    }
+
+    isEmpty(){
+        return this.size === 0 ;
+    }
+
+    prepend(value){
+
+        let node = new Node(value)
+
+        if(this.isEmpty()){
+            this.head = node ;
+        }else{
+            node.next = this.head ;
+            this.head = node ;
+        }
+
+        this.size++ ;
+    }
+
+    print(){
+
+
+        if(this.isEmpty()){
+            console.log("List is Empty...");
+        }else{
+            let listValue = ''
+            let curr = this.head
+
+            while(curr){
+                listValue += `${curr.value} `;
+                curr = curr.next
+            }
+
+            console.log(listValue)
+        }
+    }
+
+    append(value){
+        let node = new Node(value);
+
+        if(this.isEmpty()){
+            this.head = node ;
+        }else{
+
+            let curr = this.head ;
+
+            while(curr.next){
+                curr = curr.next ;
+            }
+            
+            curr.next = node 
+        }
+
+        this.size++ ;
+    }
+
+    insert(value , index){
+
+        if(index < 0 || index > this.size){
+            return ;
+        }
+        if(index === 0){
+            return this.prepend(value)
+        }
+        if(this.size === index){
+            return this.append(value)
+        }else{
+
+            let node = new Node(value)
+
+            let curr = this.head ;
+
+            for(let i=0;i< index-1 ;i++){
+                curr = curr.next
+            }
+
+            node.next = curr.next 
+            curr.next = node
+
+            this.size++ ;
+        }
+    }
+
+    removeIndex(index){
+        if(index < 0 || index >= this.size){
+            return null;
+        }
+
+        let removeNode ;
+        if(index === 0){
+            removeNode = this.head;
+            this.head = this.head.next 
+        }
+        else{
+
+            let curr =this.head ;
+
+            for(let i=0;i< index -1 ;i++){
+                curr = curr.next ;
+
+            }
+            removeNode = curr.next
+            curr.next = removeNode.next ;
+
+        }
+        this.size-- ;
+        console.log(removeNode.value)
+    }
+
+
+    removeValue(value){
+        if(this.isEmpty()){
+            return null;
+        }
+        if(this.head.value === value){
+            this.head = this.head.next ;
+            this.size-- ;
+            return value ;
+            
+        }
+        else{
+
+            let curr = this.head ;
+            let removeNode ;
+
+            while( curr.next && curr.next.value !== value ){
+                curr = curr.next ;
+            }
+           if(curr.next){
+            removeNode = curr.next ;
+            curr.next = removeNode.next ;
+            this.size-- ;
+            return removeNode.value ;
+           }
+           return null;
+
+        }
+        
+        
+    }
+
+    search(value){
+
+        if(this.isEmpty()){
+            return -1 ;
+        }else{
+
+            let curr = this.head ;
+
+            let i = 0 ;
+
+            while(curr){
+                if(curr.value === value){
+                    return i ;
+                }
+                curr = curr.next ;
+                i++ ;
+            }
+        }
+
+
+    }
+
+    
+}
+
+
+let list = new LinkedList()
+
+
+console.log(list.getSize())
+console.log(list.isEmpty())
+
+list.prepend(30)
+list.prepend(20)
+list.prepend(10)
+
+list.append(40)
+list.append(50)
+list.append(60)
+
+list.insert(35,3)
+
+list.removeIndex(2)
+
+console.log(list.removeValue(39));
+
+list.print()
+
+console.log(list.search(50))
+
+console.log(list.getSize())
