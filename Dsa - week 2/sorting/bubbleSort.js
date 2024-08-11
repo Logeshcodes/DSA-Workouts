2eb64b7974b165331d99ed34826e8c8b6c45a9e0
Logesh <logeshc18022002@gmail.com>
2024-08-11 14:17:57 +0530

    bubble

---
+++ Dsa - week 2/sorting/bubbleSort.js
@@ -0,0 +1,28 @@
+function bubbleSort(arr){
+
+    let swapped 
+
+    do {
+        swapped = false ;
+
+        for(let i=0;i< arr.length-1 ;i++){
+            if(arr[i] > arr[i+1]){
+                let temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp ;
+
+                swapped = true ;
+            }
+        }
+        
+    } while (swapped);
+
+    return arr ;
+}
+
+
+let arr = [56,89,34,55,90 , 1] ;
+
+let res = bubbleSort(arr);
+
+console.log(res);
