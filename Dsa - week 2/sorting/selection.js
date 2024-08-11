6379048e5610af8596c8cc2dd60dc9a18883989f
Logesh <logeshc18022002@gmail.com>
2024-08-11 13:50:59 +0530

    sort

---
+++ Dsa - week 2/sorting/selection.js
@@ -0,0 +1,29 @@
+function selectionSort(arr){
+
+    for(let i=0;i< arr.length;i++){
+
+        let minIndex = i ;
+
+        for(let j=i+1;j< arr.length;j++){
+
+            if(arr[j] < arr[minIndex]){
+                minIndex = j ;
+            }
+        }
+
+        if(minIndex !== i){
+            let temp = arr[i];
+            arr[i] = arr[minIndex]
+            arr[minIndex] = temp ;
+        }
+    }
+
+    return arr ;
+}
+
+
+let arr = [56,89,34,55,90]
+
+let res = selectionSort(arr);
+
+console.log(res);
