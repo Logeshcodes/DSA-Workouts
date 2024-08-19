cf15f03efe3165b23e638d04369d93baf2d62dbf
Logesh <logeshc18022002@gmail.com>
2024-08-19 11:11:14 +0530

    binary

---
+++ Dsa - week 3/tree.js
@@ -0,0 +1,49 @@
+class Node{
+    constructor(value){
+        this.value = value ;
+        this.left = null;
+        this.right = null ;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null ;
+    }
+
+    isEmpty(){
+        return this.root === null ;
+    }
+
+    insert(value){
+        let newNode = new Node(value);
+
+        if(this.isEmpty()){
+            this.root = newNode ;
+        }else{
+            this.insertNode(this.root , newNode)
+        }
+    }
+
+    insertNode(root , newNode){
+        if(root.value > newNode.value){
+            if(root.left === null){
+                root.left = newNode ;
+            }else{
+                this.insertNode(root.left , newNode)
+            }
+        }
+        else{
+            if(root.right === null){
+                root.right = newNode
+            }else{
+                this.insertNode(root.right , newNode)
+            }
+        }
+    }
+}
+
+
+const bst =  new BinarySearchTree()
+
+console.log(bst.isEmpty())
