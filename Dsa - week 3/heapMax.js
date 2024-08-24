e7965624c9c0d0447aa912247d204f4ae8dcd625
Logesh <logeshc18022002@gmail.com>
2024-08-24 07:13:01 +0530

    reviewday

---
+++ Dsa - week 3/heapMax.js
@@ -0,0 +1,95 @@
+class MaxHeap{
+    constructor(){
+        this.heap = []
+    }
+    
+    getParentIndex(index){
+        return Math.floor((index-1)/2)
+    }
+    
+    getLeftChildIndex(index){
+        return 2 * index + 1;
+    }
+    
+    getRightChildIndex(index){
+        return 2 * index + 2;
+    }
+    
+    insert(value){
+        this.heap.push(value)
+        
+        this.heapifyUp(this.heap.length-1)
+    }
+    
+    heapifyUp(index){
+        let parentIndex = this.getParentIndex(index);
+        
+        while(index > 0 && this.heap[parentIndex] < this.heap[index]){
+            [ this.heap[index] , this.heap[parentIndex]] = [this.heap[parentIndex] ,  this.heap[index]]
+        }
+        
+        index = parentIndex ;
+        parentIndex = this.getParentIndex(index)
+    }
+    
+    
+    remove(value){
+        const index = this.heap.indexOf(value);
+        if(this.heap.length === 0){
+            return null;
+        }
+        if(this.heap.length === 1){
+            return this.heap.pop();
+        }
+        
+        [this.heap[index], this.heap[this.heap.length - 1]] = [this.heap[this.heap.length - 1], this.heap[index]];
+        const removedValue = this.heap.pop();
+
+        // Heapify down from the index where the element was removed
+        this.heapifyDown(index);
+
+        return removedValue;
+    }
+    
+    heapifyDown(index){
+    let largest = index
+    let leftChildIndex = this.getLeftChildIndex(index)
+    let rightChildIndex  = this.getRightChildIndex(index)
+
+    if(leftChildIndex < this.heap.length && this.heap[leftChildIndex] > this.heap[largest] ){
+        largest = leftChildIndex
+    }
+
+    if(rightChildIndex < this.heap.length && this.heap[rightChildIndex] > this.heap[largest]){
+        largest = rightChildIndex
+    }
+
+    if(largest != index){
+        [this.heap[index] , this.heap[largest]] = [this.heap[largest] , this.heap[index]]
+        this.heapifyDown(largest)
+    }
+  }
+
+    getMax(){
+        return this.heap[0]
+    }
+    
+    print(){
+        console.log(this.heap.join(" "))
+    }
+}
+
+
+let heap = new MaxHeap()
+
+heap.insert(20)
+heap.insert(15)
+heap.insert(10)
+heap.insert(7)
+heap.insert(3)
+
+heap.remove(10)
+
+console.log(heap.getMax())
+
+heap.print()
