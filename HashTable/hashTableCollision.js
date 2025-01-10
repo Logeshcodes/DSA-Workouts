dc55f9d99364349ec3257d6274e446154927dac2
Logesh <logeshc18022002@gmail.com>
2025-01-10 22:19:01 +0530

    removed

--- HashTable/hashTableCollision.js
+++ HashTable/hashTableCollision.js
@@ -9,7 +9,7 @@ class HashTable{
         let total = 0 ;
         
         for(let i=0;i< key.length ;i++){
-            total += key.charCodeAt(i)           // i
+            total += key.charCodeAt(i)           
         }
         
         return total % this.size ; 
