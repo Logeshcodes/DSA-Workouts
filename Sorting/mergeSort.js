fb4644e864fdee306cf3806b1c02ad38739b55d4
Logesh <logeshc18022002@gmail.com>
2025-03-09 20:32:22 +0530

    cleared

--- Sorting/mergeSort.js
+++ Sorting/mergeSort.js
@@ -1,15 +1,12 @@
 function mergeSort(arr){
 
-    // merge sort
-    
     if(arr.length < 2) return arr ;
     
     let mid = Math.floor(arr.length / 2 )
 
     let leftArr = arr.slice(0, mid)
     let rightArr = arr.slice(mid)
-    
-   
+
     
     return merge(mergeSort(leftArr) , mergeSort(rightArr)) ;
     
